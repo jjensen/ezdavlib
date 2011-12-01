924dd34b10465c4a54800988cd1c8b6d88c46899
Joshua Jensen <jjensen@workspacewhiz.com>
2011-12-01 01:56:22 -0700

    Add tlsf as the backend for the test overridable memory management in davclient.

--- davclient/davclient.c
+++ davclient/davclient.c
@@ -1,5 +1,18 @@
 #include "webdav.h"
 #include "high_level_webdav_functions.h"
+#include "tlsf.h"
+
+static void *http_tlsf_allocator(void *ud, void *ptr, size_t nsize) {
+	tlsf_pool pool = (tlsf_pool)ud;
+	return tlsf_realloc(pool, ptr, nsize);
+}
+
+int usedByteCount;
+static void http_tlsf_walker(void* ptr, size_t size, int used, void* user)
+{
+	if (used)
+		usedByteCount += size;
+}
 
 int main() {
 	// Initialize Winsock.
@@ -9,12 +22,20 @@ int main() {
 	HTTP_CONNECTION* connection;
 	DAV_OPENDIR_DATA oddata;
 
+	int ret;
+	unsigned char* mem = malloc(50000);
+	tlsf_pool pool = tlsf_create(mem, 50000);
+	http_set_allocator(http_tlsf_allocator, pool);
+
 //	int ret = http_connect(&connection, "demo.sabredav.org", 80, "testuser", "test");
-//	int ret = dav_connect(&connection, "demo.sabredav.org", 80, "testuser", "test");
-	int ret = dav_connect(&connection, "localhost", 8080, NULL, NULL);
+	ret = dav_connect(&connection, "demo.sabredav.org", 80, "testuser", "test");
+//	int ret = dav_connect(&connection, "localhost", 8080, NULL, NULL);
 
 //	ret = dav_copy_to_server(connection, "d:/inffas32.obj", "/inffas32.obj");
 //	ret = dav_delete(connection, "/inffas32.obj");
+	usedByteCount = 0;
+	tlsf_walk_heap(pool, http_tlsf_walker, NULL);
+
 
 	ret = dav_opendir(connection, "/", &oddata);
 	while (ret) {
@@ -24,12 +45,20 @@ int main() {
 			break;
 		hi = 10;
 	}
+	usedByteCount = 0;
+	tlsf_walk_heap(pool, http_tlsf_walker, NULL);
+
 	dav_closedir(&oddata);
 
-//	ret = dav_copy_from_server(connection, "/public/Modern Photo Letter.pdf", "a.pdf");
-	ret = dav_copy_from_server(connection, "/document.pdf", "u:/d.pdf");
+	usedByteCount = 0;
+	tlsf_walk_heap(pool, http_tlsf_walker, NULL);
+
+	ret = dav_copy_from_server(connection, "/public/Modern Photo Letter.pdf", "a.pdf");
+	usedByteCount = 0;
+	tlsf_walk_heap(pool, http_tlsf_walker, NULL);
+	ret = dav_copy_from_server(connection, "/document.pdf", "d.pdf");
 //	ret = dav_copy_to_server(connection, "u:/screen2.jpg", "/screen2.jpg");
-	ret = dav_copy_to_server(connection, "u:/Quicken_Deluxe_2007.exe", "/qd.exe");
+//	ret = dav_copy_to_server(connection, "u:/Quicken_Deluxe_2007.exe", "/qd.exe");
 
 	dav_disconnect(&connection);
 
